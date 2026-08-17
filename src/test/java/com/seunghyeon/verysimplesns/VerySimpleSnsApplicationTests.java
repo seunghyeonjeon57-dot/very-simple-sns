54457c39bfc86f1a00f291b9fd39cbafca34b80b
전승현 <seunghyeonjeon57@gmail.com>
2026-08-17 10:08:22 +0900

    feat:프로젝트 생성

---
+++ src/test/java/com/seunghyeon/verysimplesns/VerySimpleSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seunghyeon.verysimplesns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerySimpleSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
