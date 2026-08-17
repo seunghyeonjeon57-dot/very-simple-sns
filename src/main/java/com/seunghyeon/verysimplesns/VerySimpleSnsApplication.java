3a4b8c73d6ac984b0bec35b6092b9a8539fb216f
전승현 <jeonseunghyeon@jeonseunghyeon-ui-MacBookPro.local>
2026-08-17 10:08:22 +0900

    feat:프로젝트 생성

---
+++ src/main/java/com/seunghyeon/verysimplesns/VerySimpleSnsApplication.java
@@ -0,0 +1,13 @@
+package com.seunghyeon.verysimplesns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerySimpleSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerySimpleSnsApplication.class, args);
+	}
+
+}
