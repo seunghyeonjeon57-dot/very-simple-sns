b4c50ee3e45f8f8d5064618c6ea9892bcfbd6c3e
전승현 <seunghyeonjeon57@gmail.com>
2026-08-19 11:40:45 +0900

    feat:피드 엔티티 구현

---
+++ src/main/java/com/seunghyeon/verysimplesns/domain/Feed.java
@@ -0,0 +1,49 @@
+package com.seunghyeon.verysimplesns.domain;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class Feed {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Column(nullable = false,length = 255)
+    private String content;
+
+    @Column
+    private String imageUrl;
+
+    @CreatedDate
+    @Column
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column
+    private Instant updatedAt;
+
+
+    public void updateFeed(String newContent, String newImageURl){
+        if(newContent!=null) this.content=newContent;
+        if(newImageURl!=null) this.imageUrl=newImageURl;
+    }
+
+
+}
