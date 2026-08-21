46c8928e370962d81b669a19abd6632b5d41f4ea
전승현 <seunghyeonjeon57@gmail.com>
2026-08-21 11:11:36 +0900

    feat:filterchain메서드 구현 -csrf 제외 - session제외 - filterchain허용경로 - filter순서배정

--- src/main/java/com/seunghyeon/verysimplesns/config/WebSecurityConfig.java
+++ src/main/java/com/seunghyeon/verysimplesns/config/WebSecurityConfig.java
@@ -1,18 +1,42 @@
 package com.seunghyeon.verysimplesns.config;
 
+import com.seunghyeon.verysimplesns.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+    private final JwtAuthenticationFilter filter;
 
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf->csrf.disable())
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth->auth.requestMatchers("/users/create","/auth")
+                        .permitAll().anyRequest().authenticated())
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+
+
+    }
+
 }
