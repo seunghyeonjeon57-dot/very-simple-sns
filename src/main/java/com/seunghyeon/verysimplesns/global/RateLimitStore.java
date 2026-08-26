b8f52861ba4bce26df74951a70c00030022c78a0
전승현 <seunghyeonjeon57@gmail.com>
2026-08-26 10:53:57 +0900

    feat:RateLimitStore -  write와 Read를 나눔  - ConccrentHashMap,AutomicInteger로 스레드간 침범을 방지

---
+++ src/main/java/com/seunghyeon/verysimplesns/global/RateLimitStore.java
@@ -0,0 +1,36 @@
+package com.seunghyeon.verysimplesns.global;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Component
+public class RateLimitStore {
+    private static final int LIMIT=5;
+    private final ConcurrentHashMap<UUID, AtomicInteger> writeCountMap= new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<UUID, AtomicInteger> readCountMap= new ConcurrentHashMap<>();
+
+
+    public boolean tryIncrementWrite(UUID userId){
+        return tryIncrement(writeCountMap,userId);
+    }
+    public boolean tryIncrementRead(UUID userId){
+            return tryIncrement(readCountMap,userId);
+    }
+
+    private boolean tryIncrement(ConcurrentHashMap<UUID, AtomicInteger> countMap, UUID  userId){
+        int count = countMap.computeIfAbsent(userId, k->new AtomicInteger(0)).incrementAndGet();
+        return count<=LIMIT;
+    }
+
+    public void reset(){
+        writeCountMap.clear();
+        readCountMap.clear();
+    }
+
+
+
+}
