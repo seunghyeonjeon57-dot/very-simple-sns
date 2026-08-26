ac52f9eb78a9d71e869f8793c61555ccc4ff2112
전승현 <seunghyeonjeon57@gmail.com>
2026-08-26 12:13:00 +0900

    feat-webmvcConfig에 interceptors - feed에 적용

--- src/main/java/com/seunghyeon/verysimplesns/config/WebMvcConfig.java
+++ src/main/java/com/seunghyeon/verysimplesns/config/WebMvcConfig.java
@@ -1,19 +1,30 @@
 package com.seunghyeon.verysimplesns.config;
 
 
+import com.seunghyeon.verysimplesns.interceptor.RateLimitInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
     @Value("${file.upload-dir}")
     private String dir;
+
+    private final RateLimitInterceptor interceptor;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("file:" + dir+"/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(interceptor).addPathPatterns("/feed/**");
     }
 }
