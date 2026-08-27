4ecde8bf8d25d11946e31af70eaaf6bd58dcdb39
전승현 <seunghyeonjeon57@gmail.com>
2026-08-27 11:34:12 +0900

    feat:followRepository구현

---
+++ src/main/java/com/seunghyeon/verysimplesns/repository/FollowRepository.java
@@ -0,0 +1,17 @@
+package com.seunghyeon.verysimplesns.repository;
+
+import com.seunghyeon.verysimplesns.domain.Follow;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface FollowRepository extends JpaRepository<Follow, UUID> {
+    List<Follow> findByFollowerId(UUID followerId);
+    List<Follow> findByFollowingId(UUID followingId);
+    boolean existsByFollowerIdAndFollowingId(UUID followerId,UUID FollowingId);
+
+
+}
